a38ab6aec6c200f130251888f47ef5577da86f57
ThiagoMoura963 <thiagomjfelis3@gmail.com>
2026-08-21 15:42:58 -0300

    chore: add and configure `NestJS`

---
+++ src/health.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('/api/v1/health')
+export class HealthController {
+  @Get()
+  check() {
+    return {
+      status: 'ok',
+    };
+  }
+}
