2033d36b14831fc942b3aeebcd302f23ba8f90f3
ThiagoMoura963 <thiagomjfelis3@gmail.com>
2026-08-22 20:11:02 -0300

    refactor: improve `database` error handling

--- backend/src/database/database.provider.ts
+++ backend/src/database/database.provider.ts
@@ -2,6 +2,7 @@ import { Provider } from '@nestjs/common';
 import { DATABASE_CONNECTION } from './database.constants';
 import { ConfigService } from '@nestjs/config';
 import { Pool } from 'pg';
+import { ServiceError } from 'src/errors/errors';
 
 export const databaseProvider: Provider = {
   provide: DATABASE_CONNECTION,
@@ -10,7 +11,10 @@ export const databaseProvider: Provider = {
     const databaseUrl = configService.get<string>('DATABASE_URL');
 
     if (!databaseUrl) {
-      throw new Error('Banco de dados não configurado.');
+      throw new ServiceError({
+        message: 'Banco de dados não configurado.',
+        action: 'Verifique a configuração da variável DATABASE_URL.',
+      });
     }
 
     const pool = new Pool({
